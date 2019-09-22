3da1c0fb10fc2982c0aede63218a2c0409e9feff
delbiaggionicolas <mdelbiaggio@gmail.com>
2019-09-22 21:12:43 +0200

    Adding jacoco plugin for generating test report. Updating Readme

--- src/main/java/com/example/user/api/simpleuserservice/config/SwaggerConfig.java
+++ src/main/java/com/example/user/api/simpleuserservice/config/SwaggerConfig.java
@@ -19,7 +19,7 @@ public class SwaggerConfig {
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("com.example.user.api.simpleuserservice"))
+                .apis(RequestHandlerSelectors.basePackage("com.example.user.api.simpleuserservice/api"))
                 .paths(PathSelectors.ant("/users/**"))
                 .build()
                 .apiInfo(apiInfo());
